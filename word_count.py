bee9129e235a6dd79ac7f84c8119918e82865a0e
daninick <40275764+daninick@users.noreply.github.com>
2020-11-23 14:53:32 +0200

    Good comments - I suppose

--- word_count.py
+++ word_count.py
@@ -9,7 +9,6 @@ Enter a .txt file name: ''')
 f = open(filename, 'r')
 
 # Read the file and convert it to a string
-
 text = f.read()
 
 # Ignore the empty lines
@@ -17,7 +16,6 @@ text = text.replace('\n', ' ')
 
 # All words in the text are separated by space now.
 # Make a list of all words in the text using regular expressions.
-
 words = re.findall(r'\w+',text,re.I)
 
 # Create an empty dictionary
@@ -26,13 +24,11 @@ d = {}
 # For every word in the list of words check if the word exists in the dictionary.
 # If it doesn't exist -> add it as a key and set a count of 1 as a value.
 # If the word exists in the dictionary -> simply increase the count with 1.
-
-
 for word in words:
     word = word.lower()
+
 # finds the items that contain a digit and do not put them in the dictionary.
 # I presume the words has only word characters and no numbers
-
     match = re.search(r'\d',word)
     if match:
         pass
@@ -43,7 +39,6 @@ for word in words:
             d[word] = d[word] + 1
 
 # Prints on the terminal all words and their count even the count of the keywords, alphabetically sorted.
-
 print('''
 
 The words in the file are: ''')
